2873dcfc0c84c02f194a2329e3dbe0ef1f2f641e
Ian Njari <iannjari@gmail.com>
2024-07-18 17:00:58 +0300

    add jpa and spring kafka deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,12 @@ dependencies {
     // mariaDB
     implementation("org.mariadb.jdbc:mariadb-java-client:3.4.0")
 
+    // spring data JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.1")
+
+    // kafka
+    implementation("org.springframework.kafka:spring-kafka:3.2.2")
+
     // tests
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
